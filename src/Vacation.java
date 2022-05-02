fa8970927c2418acb6fe133726392a02b69986cf
Simone Mayers <53239742+simonemayers@users.noreply.github.com>
2022-05-02 10:34:15 -0400

    done

---
+++ src/Vacation.java
@@ -0,0 +1,7 @@
+public class Vacation{
+    public static void main(String[] args){
+        System.out.println("I'd like to visit"); 
+        System.out.println("Aukland"); 
+        System.out.println("New Zealand");
+    }
+}
